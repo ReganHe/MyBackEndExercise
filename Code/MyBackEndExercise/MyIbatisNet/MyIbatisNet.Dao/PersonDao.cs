02adece940416e0c3ec4aad3ad336166c0bdcb65
hjdev_heke <heke@hujiang.com>
2016-01-07 10:47:12 +0800

    新增IbatisNet项目

---
+++ Code/MyBackEndExercise/MyIbatisNet/MyIbatisNet.Dao/PersonDao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBatisNet.DataMapper;
+using MyIbatisNet.Domain;
+
+namespace MyIbatisNet.Dao
+{
+    public class PersonDao
+    {
+        public IList<PersonModel> GetList()
+        {
+            ISqlMapper mapper = Mapper.Instance();
+            IList<PersonModel> listPerson = mapper.QueryForList<PersonModel>("SelectAllPerson", null);  //这个"SelectAllPerson"就是xml映射文件的Id
+            return listPerson;
+        }
+    }
+}
---
+++ Code/MyBackEndExercise/MyIbatisNet/MyIbatisNet.FunctionTests/PersonDaoTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyIbatisNet.Dao;
+using MyIbatisNet.Domain;
+
+namespace MyIbatisNet.FunctionTests
+{
+    [TestClass]
+    public class PersonDaoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var dao = new PersonDao();
+            var listPerson = dao.GetList();
+            Assert.IsTrue(listPerson.Count > 0);
+        }
+    }
+}
